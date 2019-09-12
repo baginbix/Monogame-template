21cb1c9fb52727a0fe8ba4816a4733a91a1f7a2b
baginbix <timtuvestam@hotmail.se>
2019-09-12 13:55:08 +0200

    fd

--- Template/Template/Game1.cs
+++ Template/Template/Game1.cs
@@ -40,7 +40,7 @@ namespace Template
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            // TODO: use this.Content to load your game content here 
         }
 
         /// <summary>
